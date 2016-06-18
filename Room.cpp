e8b4ee3556a652bef17d1ba2d7e87b3ddef34acd
harel9123 <harelkick@gmail.com>
2016-06-18 19:24:43 +0300

    Revert "Revert "השינויים שאתה עשית לROOM""
    This reverts commit ef155cdf9675548ae4853c721dc82aa8be6c6234.

--- Room.cpp
+++ Room.cpp
@@ -15,14 +15,39 @@ bool Room::joinRoom(User *)
 
 }
 
-void Room::leaveRoom(User *)
+void Room::leaveRoom(User *user)
 {
-
+	int i = 0;
+	for (vector<User *>::iterator it = _users.begin(); it != _users.end(); ++it)
+	{
+		if (it.operator != *user)
+		{
+			_users.erase(_users.begin() + i); //need to take care about sendMessage because i didnt understand it
+		}
+		i++;
+	}
 }
 
-int Room::closeRoom(User *)
+int Room::closeRoom(User *user)
 {
-
+	int retVal;
+	if (user == _admin)
+	{
+		//need to take care about sendMessage because i didnt understand it
+		retVal = _id;
+		for (int i = 0; i < _users.size(); i++)
+		{
+			if (_users[i] != _admin)
+			{
+				_users[i].clearRoom();
+			}
+		}
+	}
+	else
+	{
+		retVal = -1;
+	}
+	return retVal;
 }
 
 string Room::getUsersListMessage()
@@ -30,9 +55,17 @@ string Room::getUsersListMessage()
 
 }
 
-string Room::getUsersAsString(vector<User *>, User *)
+string Room::getUsersAsString(vector<User *>users, User *user)
 {
-
+	string usersStr;
+	for (vector<User *>::iterator it = users.begin(); it != users.end(); ++it)
+	{
+		if (it.operator != *user)
+		{
+			usersStr = *it.operator*.getUsername(); //not sure
+		}
+	}
+	return usersStr;
 }
 
 void Room::sendMessage(string)
